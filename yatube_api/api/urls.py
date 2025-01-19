13489578d797f2a0626e8a366ec33de1d77c6b82
Fedorenko Ilia <fedorenkoilia@gmail.com>
2025-01-19 13:51:17 +0100

    1

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -8,6 +8,8 @@ router.register('groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('posts/<int:post_id>/comments/', CommentViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('posts/<int:post_id>/comments/<int:pk>/', CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
+    path('posts/<int:post_id>/comments/', CommentViewSet.as_view(
+        {'get': 'list', 'post': 'create'})),
+    path('posts/<int:post_id>/comments/<int:pk>/', CommentViewSet.as_view(
+        {'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
 ]
